1174c14174f9c97532e118717d1faea91c016bb5
afalkon <info.dryguy@gmail.com>
2023-10-25 13:37:55 +0200

    FAQ added

--- html/resources/views/index.blade.php
+++ html/resources/views/index.blade.php
@@ -51,9 +51,51 @@
 
 <!--    Шестая секция: FAQ -->
 
-    <section class="faq">
+    <section class="faq" id="faq">
         <div class="container">
-
+            <div class="faq__inner">
+                <h2 class="global__heading">
+                    FAQ
+                </h2>
+                <div class="faq__wrapper">
+                    <div class="faq__item expanded">
+                        <div class="faq__header">
+                            <h3>Qu’est-ce qu’un défibrillateur ?</h3>
+                            <div class="faq__status--icon"></div>
+                        </div>
+                        <div class="faq__content">
+                            <p>Le viager est une transaction immobilière entre un acheteur (débirentier) et un vendeur (crédirentier). Le débirentier verse un bouquet et une rente à vie au crédirentier en échange de la jouissance du bien. Ceci, soit en viager libre, l'acheteur peut occuper le bien immédiatement, soit en viager occupé, le vendeur conserve le droit d'usage et d'habitation.</p>
+                        </div>
+                    </div>
+                    <div class="faq__item">
+                        <div class="faq__header">
+                            <h3>A quoi sert un défibrillateur ?</h3>
+                            <div class="faq__status--icon"></div>
+                        </div>
+                        <div class="faq__content">
+                            <p>Acheter en viager peut s'avérer très avantageux pour différents profils d'investisseurs. C'est une opportunité d'acquérir un bien immobilier et de constituer progressivement un patrimoine sans recourir à un crédit et avec des frais bancaires réduits. De plus, cette transaction offre la possibilité d'offrir un soutien financier à un senior tout en bénéficiant d'avantages fiscaux intéressants.</p>
+                        </div>
+                    </div>
+                    <div class="faq__item">
+                        <div class="faq__header">
+                            <h3>Qu’est-ce qu’un défibrillateur ?</h3>
+                            <div class="faq__status--icon"></div>
+                        </div>
+                        <div class="faq__content">
+                            <p>Les pièges d'une vente en viager peuvent inclure une évaluation inappropriée du bouquet initial ou de la rente ainsi que des risques d'impayés de la rente. Pour éviter ces problèmes, il est crucial de bien comprendre les termes du contrat et de consulter des <b>experts en viager</b>.</p>
+                        </div>
+                    </div>
+                    <div class="faq__item">
+                        <div class="faq__header">
+                            <h3>Comment se servir d’un défibrillateur ?</h3>
+                            <div class="faq__status--icon"></div>
+                        </div>
+                        <div class="faq__content">
+                            <p>Il n'y a pas de limite d'âge spécifique, <b>vendre en viager à 70 ans</b>, 50 ans ou 85 ans est possible. Généralement, cela est plus courant chez les seniors qui cherchent à compléter leur retraite ou à bénéficier d'un capital immédiat. Toutefois, l'âge du crédirentier peut influencer le montant de la rente viagère, du bouquet et de la fiscalité applicable à la vente.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 
