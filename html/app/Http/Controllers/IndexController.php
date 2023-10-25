abf836da0df6deb1ef07caa88cf70ac5a3f43b6f
afalkon <info.dryguy@gmail.com>
2023-10-25 14:04:03 +0200

    FAQ, Contact, DAE-DSA pages added (Backend)

--- html/app/Http/Controllers/IndexController.php
+++ html/app/Http/Controllers/IndexController.php
@@ -2,14 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class IndexController extends Controller
 {
+    private $description = "Défibrillateur en Location ou à l'Achat avec une Formation à Distance, Livraison sous 48H. N'hésitez pas à demander un Devis Réponse dans la Journée &#9200;";
+    private $keywords = "défibrillateur cardiaque";
+    private $canonical;
+
+    public function __construct()
+    {
+        $this->canonical = route('index');
+    }
+
     public function __invoke()
     {
         $data = [
-
+            'description' => $this->description,
+            'keywords' => $this->keywords,
+            'canonical' => $this->canonical,
         ];
 
         return view('index', compact('data'));
