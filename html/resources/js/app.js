e6b0ac353a93daad8466705b96c9c4175f76d278
Tomasinochka <leotomasino1337@gmail.com>
2023-10-25 15:51:44 +0300

    Home Page

--- html/resources/js/app.js
+++ html/resources/js/app.js
@@ -1,1 +1,17 @@
+const buttons = document.querySelectorAll('.sale__slider button');
+const frames = document.querySelectorAll('.sale__content');
 
+
+if (buttons.length > 0) {
+    buttons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+          buttons.forEach(btn => btn.classList.remove('sale__button__active'));
+        
+          frames.forEach(frame => frame.classList.remove('sale__active__frame'));
+        
+          button.classList.add('sale__button__active');
+        
+          frames[index].classList.add('sale__active__frame');
+        });
+    });
+}
